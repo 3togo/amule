d0b4adf9821c4395811f3e46137cb26c2f3448be
eli <eli@TRX40>
2026-01-26 18:54:13 +0800

    Remove cpp20_build from git tracking

--- cpp20_build/unittests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eli/git/amule/unittests
-# Build directory: /home/eli/git/amule/cpp20_build/unittests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("muleunit")
-subdirs("tests")
--- cpp20_build/unittests/muleunit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eli/git/amule/unittests/muleunit
-# Build directory: /home/eli/git/amule/cpp20_build/unittests/muleunit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
